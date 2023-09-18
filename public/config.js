f344c65e55de1f9bc09d641450286486d6d96c56
chelmtv <chelmtv@proton.me>
2023-09-17 21:35:38 -0700

    k

---
+++ public/config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://chelmtv.chelmtv.workers.dev",
+  VITE_TMDB_READ_API_KEY: "6963cb3049705a8e9789f45f47085eb7"
+};
